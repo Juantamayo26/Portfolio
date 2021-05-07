6b31d492c75c17f3da5da69cd94de94906db7a79
Juan Tamayo <juancyepest@gmail.com>
2021-05-06 19:25:34 -0500

    Added responsive site in porjects

--- src/components/Project.js
+++ src/components/Project.js
@@ -3,18 +3,19 @@ import Header from "./Header";
 import Img from "gatsby-image";
 
 const CodeArtPreview = ({ title, image, type, description }) => {
-  //if (type % 2 == 0) {
+  if (type % 2 == 0) {
     return (
       <div
         className={
           "flex flex-wrap md:flex-nowrap w-full justify-center md:justify-end items-center"
         }
       >
-        <div className="flex-wrap justify-end w-3/4 max-w-lg ml-4 mr-8 text-right md:flex md:w-1/2 xl:w-1/3">
+        <div className="flex-wrap justify-start max-w-lg text-left md:w-3/4 md:ml-8 md:mr-4 md:flex md:w-1/2 xl:w-1/3">
           <Header variant="4">{title}</Header>
           <div className="flex w-full mt-4 mb-4 boxshadow-3d-right">
-            <div className="w-full m-4 text-sm text-white prose md:text-md lg:text-lg"
-              dangerouslySetInnerHTML={{__html: description}}
+            <div
+              className="w-full m-4 text-sm text-white prose md:text-md lg:text-lg"
+              dangerouslySetInnerHTML={{ __html: description }}
             />
           </div>
         </div>
@@ -28,32 +29,40 @@ const CodeArtPreview = ({ title, image, type, description }) => {
         </div>
       </div>
     );
-  //} else {
-  //  return (
-  //    <div
-  //      className={
-  //        "flex flex-wrap md:flex-nowrap w-full justify-center md:justify-start items-center"
-  //      }
-  //    >
-  //      <div className="flex justify-center w-3/4 mx-4 md:w-1/2 xl:w-1/3 md:justify-end">
-  //        <Img
-  //          title={title}
-  //          fluid={image}
-  //          className="w-full picture-border-sm-2"
-  //          alt={title + " Preview Image"}
-  //        />
-  //      </div>
-  //      <div className="flex-wrap justify-start w-3/4 max-w-lg ml-8 mr-4 text-left md:flex md:w-1/2 xl:w-1/3">
-  //        <Header variant="1">{title}</Header>
-  //        <div className="flex w-full mt-4 mb-4 boxshadow-3d-left">
-  //          <p className="w-full m-4 text-sm md:text-md lg:text-lg font-extralight font-manrope">
-  //            {description}
-  //          </p>
-  //        </div>
-  //      </div>
-  //    </div>
-  //  );
-  //}
+  } else {
+    return (
+      <div
+        className={
+          "flex flex-wrap md:flex-nowrap w-full justify-center md:justify-start items-center"
+        }
+      >
+        <div className="flex justify-center hidden w-3/4 mx-4 md:block md:w-1/2 xl:w-1/3 md:justify-end">
+          <Img
+            title={title}
+            fluid={image}
+            className="w-full picture-border-sm-2"
+            alt={title + " Preview Image"}
+          />
+        </div>
+        <div className="flex-wrap justify-start max-w-lg text-left md:w-3/4 md:ml-8 md:mr-4 md:flex md:w-1/2 xl:w-1/3">
+          <Header variant="1">{title}</Header>
+          <div className="flex w-full mt-4 mb-4 boxshadow-3d-left">
+            <p className="w-full m-4 text-sm md:text-md lg:text-lg font-extralight font-manrope">
+              {description}
+            </p>
+          </div>
+        </div>
+        <div className="flex justify-center w-3/4 mx-4 md:hidden md:w-1/2 xl:w-1/3 md:justify-end">
+          <Img
+            title={title}
+            fluid={image}
+            className="w-full picture-border-sm-2"
+            alt={title + " Preview Image"}
+          />
+        </div>
+      </div>
+    );
+  }
 };
 
 export default CodeArtPreview;
